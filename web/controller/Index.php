147905a81a23897eea0c6a517f3e70e304037a82
司海清 <hc13908252253@163.com>
2018-07-08 17:30:10 +0800

    创建测试控制器

---
+++ web/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haiqing si
+ * Date: 2018/7/8
+ * Time: 17:27
+ */
+
+namespace web\controller;
+
+
+class Index
+{
+public function show(){
+    echo 'hc13908252253..........';
+}
+}
